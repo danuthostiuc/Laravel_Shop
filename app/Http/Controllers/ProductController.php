656c3252c6834fac42b3b0bb7258b6b393fe07aa
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-02-22 16:44:15 +0200

    - the name of the images are now stored as a concatenation of uniqid() and their extension.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -59,7 +59,7 @@ class ProductController extends Controller
 
             $file = Input::file('image');
             $destinationPath = public_path() . '/storage/';
-            $filename = uniqid() . $file->getClientOriginalName();
+            $filename = uniqid() . '.' . $file->getClientOriginalExtension();
             $file->move($destinationPath, $filename);
 
             Product::query()->where('id', \request('id'))
